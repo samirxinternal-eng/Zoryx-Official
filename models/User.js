8b7f5337a8b678694888182833a281d47fcbca33
samirxinternal-eng <samirxinternal@gmail.com>
2026-08-07 10:26:18 +0600

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,15 @@
+const mongoose = require('mongoose');
 
+const userSchema = new mongoose.Schema({
+  telegramId: { type: String, required: true, unique: true },
+  username: { type: String, default: '' },
+  firstName: { type: String, default: '' },
+  coins: { type: Number, default: 0 },
+  referredBy: { type: String, default: null },
+  referralCount: { type: Number, default: 0 },
+  completedTasks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Task' }],
+  lastAdWatch: { type: Date, default: null },
+  joinedAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('User', userSchema);
