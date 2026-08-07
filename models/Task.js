27a14299bfac1ed5fbe72844597969e92e2c304f
samirxinternal-eng <samirxinternal@gmail.com>
2026-08-07 10:25:52 +0600

    Update Task.js

--- models/Task.js
+++ models/Task.js
@@ -1,1 +1,16 @@
+const mongoose = require('mongoose');
 
+const taskSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  url: { type: String, default: '' },
+  reward: { type: Number, required: true },
+  type: {
+    type: String,
+    enum: ['channel', 'link', 'bot'],
+    default: 'link'
+  },
+  active: { type: Boolean, default: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Task', taskSchema);
