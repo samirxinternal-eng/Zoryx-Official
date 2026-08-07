686fe3c39798d5ceb3e55e6e6353a70444475f0a
samirxinternal-eng <samirxinternal@gmail.com>
2026-08-07 14:13:42 +0600

    Update api.js

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,1 +1,38 @@
+const express = require('express');
+const router = express.Router();
 
+const {
+  requireTelegramAuth,
+  getMe,
+  setLanguage,
+  getLeaderboard,
+  watchAd,
+} = require('../controllers/userController');
+
+const {
+  listTasks,
+  createTask,
+  deleteTask,
+  startTask,
+  checkTask,
+} = require('../controllers/taskController');
+
+const { MONETAG_ZONE_ID } = require('../config');
+
+router.use(requireTelegramAuth);
+
+// user
+router.get('/me', getMe);
+router.post('/language', setLanguage);
+router.get('/leaderboard', getLeaderboard);
+router.post('/ads/watch', watchAd);
+router.get('/config', (req, res) => res.json({ monetagZoneId: MONETAG_ZONE_ID }));
+
+// tasks
+router.get('/tasks', listTasks);
+router.post('/tasks', createTask);
+router.delete('/tasks/:id', deleteTask);
+router.post('/tasks/go', startTask);
+router.post('/tasks/check', checkTask);
+
+module.exports = router;
