d5d95c1a5b20ec2bcf5401af400d3b285ef1a9dc
samirxinternal-eng <samirxinternal@gmail.com>
2026-08-11 16:27:45 +0600

    Update help.js

--- src/bot/commands/help.js
+++ src/bot/commands/help.js
@@ -1,19 +1,23 @@
 const User = require('../../models/User');
 const { t } = require('../../locales');
-const { isAdmin } = require('../middlewares/adminCheck');
+const { isAdmin, isOwner } = require('../middlewares/adminCheck');
 const { DEFAULT_LANG } = require('../../config');
 
 module.exports = async function helpCommand(ctx) {
   const telegramId = String(ctx.from.id);
 
-  const admin = await isAdmin(telegramId);
+  const owner = isOwner(telegramId);
+  const admin = owner || (await isAdmin(telegramId));
   if (!admin) {
-    // IMPORTANT: regular users get NO response at all for /help
+    // regular users get NO response at all for /help
     return;
   }
 
   const user = await User.findOne({ telegramId });
   const locale = t((user && user.language) || DEFAULT_LANG);
 
-  return ctx.replyWithMarkdown(`${locale.helpTitle}\n\n${locale.helpBody}`);
+  if (owner) {
+    return ctx.replyWithMarkdown(`${locale.ownerHelpTitle}\n\n${locale.ownerHelpBody}`);
+  }
+  return ctx.replyWithMarkdown(`${locale.adminHelpTitle}\n\n${locale.adminHelpBody}`);
 };
