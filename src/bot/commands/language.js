78d1a2860b8d097475047536c66ef42d067bd518
samirxinternal-eng <samirxinternal@gmail.com>
2026-08-11 16:28:31 +0600

    Update language.js

--- src/bot/commands/language.js
+++ src/bot/commands/language.js
@@ -1,31 +1,27 @@
 const User = require('../../models/User');
 const { t } = require('../../locales');
-const { languageKeyboard, mainMenuKeyboard } = require('../keyboards');
-const { DEFAULT_LANG } = require('../../config');
+const { languageKeyboard } = require('../keyboards');
+const { DEFAULT_LANG, OFFICIAL_CHANNEL, COMMUNITY_CHANNEL } = require('../../config');
 
 async function setLanguageAction(ctx) {
-  const code = ctx.match[1]; // from regex setlang_(bn|en|hi|ar)
+  const code = ctx.match[1]; // from regex setlang_(en|zh|ru|ar|fr|pt|es|vi|bn)
   const telegramId = String(ctx.from.id);
 
-  const user = await User.findOneAndUpdate(
-    { telegramId },
-    { language: code },
-    { new: true, upsert: true }
-  );
+  await User.findOneAndUpdate({ telegramId }, { language: code }, { new: true, upsert: true });
 
   const locale = t(code);
   await ctx.answerCbQuery(locale.languageSet);
 
   const displayName = ctx.from.first_name || ctx.from.username || 'Friend';
-  await ctx.editMessageText(
-    locale.welcome(displayName),
-    { parse_mode: 'Markdown', ...mainMenuKeyboard(locale) }
-  );
+  // Replace the marketing/picker message with the detailed welcome message,
+  // then send the language buttons again underneath so the user can switch anytime.
+  await ctx.editMessageText(locale.welcome(displayName), { parse_mode: 'Markdown' });
+  return ctx.reply(locale.welcomeLinks(OFFICIAL_CHANNEL, COMMUNITY_CHANNEL), languageKeyboard());
 }
 
 async function changeLanguageAction(ctx) {
   await ctx.answerCbQuery();
-  await ctx.editMessageText(t(DEFAULT_LANG).chooseLanguage, languageKeyboard());
+  return ctx.reply(t(DEFAULT_LANG).chooseLanguage, languageKeyboard());
 }
 
 module.exports = { setLanguageAction, changeLanguageAction };
