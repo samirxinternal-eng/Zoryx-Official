f9def498b3f8579d307449750c2aba87b977716f
samirxinternal-eng <samirxinternal@gmail.com>
2026-08-11 16:39:41 +0600

    Update index.js

--- src/locales/index.js
+++ src/locales/index.js
@@ -1,12 +1,17 @@
-const bn = require('./bn');
 const en = require('./en');
-const hi = require('./hi');
+const zh = require('./zh');
+const ru = require('./ru');
 const ar = require('./ar');
+const fr = require('./fr');
+const pt = require('./pt');
+const es = require('./es');
+const vi = require('./vi');
+const bn = require('./bn');
 
-const locales = { bn, en, hi, ar };
+const locales = { en, zh, ru, ar, fr, pt, es, vi, bn };
 
 function t(lang) {
-  return locales[lang] || locales.bn;
+  return locales[lang] || locales.en;
 }
 
 module.exports = { t, locales };
